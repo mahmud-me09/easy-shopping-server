a90753a93301d97241e4db1bb1f7d5cce5c7c9be
Mahmud Hasan Khan <mahmud.me09@gmail.com>
2024-08-18 15:21:38 +0600

    fixing deployment issue

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 // middlewares
 
-app.use(express.json())
+app.use(express.json()) 
 app.use(cors())
 app.use((req, res, next) => {
 	res.header({ "Access-Control-Allow-Origin": "*" });
